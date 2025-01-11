0d24c0f915bcf6dda3bfaafb20f99fb244d547f3
awsajju <aws.devops.123aws@gmail.com>
2025-01-11 20:53:15 +0530

    function dynamically installing  package

---
+++ function.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+check_root(){
+    if [ $USERID -ne 0 ];then
+        echo "You Must have the root access to execute this"
+        exit 1
+    fi
+}
+
+check_root
+
+validation(){
+    if [ $? -ne 0 ];then
+        echo " $2 installing package is failure"
+        exit 1
+    else
+        echo " $2 installing package is success"
+    fi
+}
+
+package=$1
+
+dnf list installed $1
+    if [$? -ne 0 ];then
+        dnf install $1 -y
+            validate $? "installing $1"
+    else
+        echo "package alaready available"
+    fi
